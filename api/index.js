17de35995517356b5493b6807c534d1cf9d737cb
ValentinAllende <valentin_allende@hotmail.com>
2023-07-08 15:24:47 -0300

    deploy vercel config 1

--- api/index.js
+++ api/index.js
@@ -5,6 +5,6 @@ app.set("port", 3000);
 app.listen(app.get("port"), () => {
   // este force true busca las tablas, si estan als dropea y las vuelve a generar, se puede cambiar a alter
   //alter :true SOLO actualiza las tablas y no las esta dropeando todo el tiempo
-  sequelize.sync({ alter: true });
+  sequelize.sync({ force: true });
   console.log(`Servidor escuchando en puerto ${app.get("port")}`);
 });
