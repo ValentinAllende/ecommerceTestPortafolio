ee8babcea2cb5ecc4971eebf7b082a9d2b9f9a2e
ValentinAllende <valentin_allende@hotmail.com>
2023-07-09 16:56:03 -0300

    act gitignore y db url

--- api/db.js
+++ api/db.js
@@ -3,12 +3,8 @@ const productModel = require("./src/models/product");
 const categoryModel = require("./src/models/category");
 
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env;
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`,
-  { logging: false }
-  //postgres://alej:lxKqezUkfWL55LLIDviUU1zbiOrk7f3s@dpg-cilg84dph6eg6k9arua0-a.oregon-postgres.render.com/dbecommerce_q292
-);
+const { DB_URL } = process.env;
+const sequelize = new Sequelize(`${DB_URL}`, { logging: false });
 //{ logging: false } esto si esta en true muestra lo que devolveria la db, puede servir para test
 /*MODELOS*/
 productModel(sequelize);
