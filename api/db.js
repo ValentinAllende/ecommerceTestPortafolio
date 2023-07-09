6bed02fca6576fbd115570f2ea3597d115d9b5d4
ValentinAllende <valentin_allende@hotmail.com>
2023-07-09 16:25:31 -0300

    database config prod

--- api/db.js
+++ api/db.js
@@ -3,10 +3,11 @@ const productModel = require("./src/models/product");
 const categoryModel = require("./src/models/category");
 
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env;
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommercep`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`,
   { logging: false }
+  //postgres://alej:lxKqezUkfWL55LLIDviUU1zbiOrk7f3s@dpg-cilg84dph6eg6k9arua0-a.oregon-postgres.render.com/dbecommerce_q292
 );
 //{ logging: false } esto si esta en true muestra lo que devolveria la db, puede servir para test
 /*MODELOS*/
